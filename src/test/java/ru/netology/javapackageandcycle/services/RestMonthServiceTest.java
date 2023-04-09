0a7e39b1563f5745f4182ae3d4a367ba3c4b49cc
AnnWolkova <volkov.nikita.a@gmai.com>
2023-04-10 00:32:04 +0700

    parameterizedTest

--- src/test/java/ru/netology/javapackageandcycle/services/RestMonthServiceTest.java
+++ src/test/java/ru/netology/javapackageandcycle/services/RestMonthServiceTest.java
@@ -1,23 +1,28 @@
 package ru.netology.javapackageandcycle.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestMonthServiceTest {
-    @Test
-    public void ShouldCalcRestMonth1 ()
-    { RestMonthService service = new RestMonthService();
-        int expected = 3; // данные подготавлваем
-        int actual = service.calculate(10_000,3_000,20_000); // вызывается целевой метод
-        Assertions.assertEquals(expected,actual); //проверка сравнения ожидаемого и фактического результатов
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/RestMonth.csv")
 
-    }
-    @Test
-    public void ShouldCalcRestMonth2 ()
-    { RestMonthService service = new RestMonthService();
-        int expected = 2; // данные подготавлваем
-        int actual = service.calculate(100_000,60_000,150_000); // вызывается целевой метод
-        Assertions.assertEquals(expected,actual); //проверка сравнения ожидаемого и фактического результатов
+    public void ShouldCalcRestMonth(int expected, int income, int expenses, int threshold) {
+        RestMonthService service = new RestMonthService();
+//        int expected = 3; // данные подготавлваем
+        int actual = service.calculate(income, expenses, threshold);  // вызывается целевой метод
+        Assertions.assertEquals(expected, actual); //проверка сравнения ожидаемого и фактического результатов
 
     }
 }
+//    @Test
+//    public void ShouldCalcRestMonth2 ()
+//    { RestMonthService service = new RestMonthService();
+//        int expected = 2; // данные подготавлваем
+//        int actual = service.calculate(100_000,60_000,150_000); // вызывается целевой метод
+//        Assertions.assertEquals(expected,actual); //проверка сравнения ожидаемого и фактического результатов
+//
+
+
 
